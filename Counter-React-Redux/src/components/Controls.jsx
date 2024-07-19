981667f0a75b3efe2f384c005cc2f42f50101ff6
13virat <viratgupta1314@gmail.com>
2024-07-20 01:03:47 +0530

    first commit

---
+++ Counter-React-Redux/src/components/Controls.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+
+const Controls = () => {
+  const dispatch=useDispatch();
+  const increment=()=>{
+    dispatch({type:'INCREMENT'});
+  }
+  const decrement=()=>{
+    dispatch({type:'DECREMENT'});
+  }
+  return (
+    <div classNameName="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      
+      <button type="button" className="btn btn-primary spc" onClick={increment}>
+       +1
+      </button>
+      <button type="button" className="btn btn-success" onClick={decrement}>
+        -1
+      </button>
+    </div>
+  );
+};
+export default Controls;
